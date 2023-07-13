9cc39988d497c63e06558e6faea744910d92de17
danish <danishjamadar9089@gmail.com>
2023-07-13 20:03:11 +0530

    add3

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,45 @@
-
-import React, { useState } from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [name,setName] = useState("");
-  function change(e) {
-    // setName("Hello "+ e.target.value + "!");
-    setName(`Hello ${e.target.value}!`);
+
+const[greet,setGreet]=useState();
+  const handleChange=(event)=>{
+    console.log(event.target.value);
+    setGreet(event.target.value);
   }
-
-
   return (
     <div>
-      {/* Do not remove the main div */}
-      <p>Enter your name:</p>
-      <input type="text" onChange={change} />
-      <p>{name}</p>
+        {/* Do not remove the main div */}
+        Enter your name: 
+        <br/>
+        <input type="text" onChange={handleChange}/>
+        <p>{greet && `Hello ${greet}!`}</p>
     </div>
   )
 }
 
 export default App
+
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   const [name,setName] = useState("");
+//   function change(e) {
+//     // setName("Hello "+ e.target.value + "!");
+//     setName(`Hello ${e.target.value}!`);
+//   }
+
+
+//   return (
+//     <div>
+//       {/* Do not remove the main div */}
+//       <p>Enter your name:</p>
+//       <input type="text" onChange={change} />
+//       <p>{name}</p>
+//     </div>
+//   )
+// }
+
+// export default App
